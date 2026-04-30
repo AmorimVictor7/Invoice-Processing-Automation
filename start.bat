ec8c5c7939afade5d655d0967cff0a90b4fc5744
Victor Amorim <amorimvictor511@gmail.com>
2026-04-30 14:51:01 -0300

    remove start.bat

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-echo Iniciando backend...
-start "Backend" cmd /k "cd backend && venv\Scripts\activate && uvicorn main:app --reload"
-
-echo Iniciando frontend...
-start "Frontend" cmd /k "cd frontend && (if not exist node_modules npm install) && npm run dev"
